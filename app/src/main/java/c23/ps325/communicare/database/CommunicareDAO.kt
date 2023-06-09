087f119f4ff64f74b3f6e341db337c3f0e5fc168
Richard Lois <richard.lois@ti.ukdw.ac.id>
2023-06-09 19:35:54 +0700

    Make DAO and DB for storing history

---
+++ app/src/main/java/c23/ps325/communicare/database/CommunicareDAO.kt
@@ -0,0 +1,19 @@
+package c23.ps325.communicare.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface CommunicareDAO {
+    @Query("SELECT * FROM PredictionHistory SORT BY date DESC")
+    fun getAllHistory(): LiveData<List<PredictionHistory>>
+
+    @Insert
+    fun addHistory(history: PredictionHistory)
+
+    @Delete
+    fun deleteHistory(history: PredictionHistory)
+}
